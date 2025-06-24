c76264822b6eac9c3ee51477f236f79d964c74b0
Fisher48 <rybakov48@yandex.ru>
2025-06-24 15:41:31 +0300

    Added necessary checks in Task2

--- Part_1/src/main/java/ru/fisher/task2/BankAccount.java
+++ Part_1/src/main/java/ru/fisher/task2/BankAccount.java
@@ -1,6 +1,10 @@
 package ru.fisher.task2;
 
+import java.util.logging.Logger;
+
 public class BankAccount {
+
+    Logger log = Logger.getLogger(BankAccount.class.getName());
 
     private double balance;
 
@@ -9,10 +13,22 @@ public class BankAccount {
     }
 
     public void deposit(int amount) {
+        if (amount < 0) {
+            log.warning("Нельзя внести отрицательную сумму");
+            return;
+        }
         this.balance += amount;
     }
 
     public void withdraw(int amount) {
+        if (amount < 0) {
+            log.warning("Нельзя снять отрицательное значение");
+            return;
+        }
+        if (this.balance < amount) {
+            log.warning("На счете недостаточно средств для снятия");
+            return;
+        }
         this.balance -= amount;
     }
 
@@ -23,21 +39,32 @@ public class BankAccount {
 
     public static void main(String[] args) {
         // Создаем счет
-        BankAccount bankAccount = new BankAccount(0);
+        BankAccount bankAccount = new BankAccount(5000);
         System.out.println("Изначальный баланс: " + bankAccount.getBalance());
 
         // Пополняем счет на некоторую сумму
         bankAccount.deposit(100);
         bankAccount.deposit(500);
-        System.out.println("Текущий баланс после добавления 100 и 500: " + bankAccount.getBalance());
+        System.out.println("Текущий баланс после добавления 100 и 500: "
+                + bankAccount.getBalance());
+
+        // Снимаем обычную сумму в пределах баланса счета
+        bankAccount.withdraw(1000);
+        System.out.println("Текущий баланс после снятия 1000: "
+                + bankAccount.getBalance());
 
         // Снимаем со счета сумму, больше чем есть на счете
-        bankAccount.withdraw(1000);
-        System.out.println("Текущий баланс, после снятия 1000: " + bankAccount.getBalance());
+        bankAccount.withdraw(6000);
+        System.out.println("Текущий баланс, после попытки снять 6000: "
+                + bankAccount.getBalance());
 
-        // Вносим очень большую сумму
-        bankAccount.deposit(1000000000);
-        System.out.println("Текущий баланс, после добавления очень большого числа: " + bankAccount.getBalance());
+        bankAccount.deposit(-100); // Некорректный депозит
+        System.out.println("Баланс после некорректного депозита -100: "
+                + bankAccount.getBalance());
+
+        bankAccount.withdraw(-50); // Некорректное снятие
+        System.out.println("Баланс после некорректного снятия -50: "
+                + bankAccount.getBalance());
     }
 
 }
